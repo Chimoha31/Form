45afe6d9eedc9ff227085f71280ef1b032d71ee8
Chimoha31 <mokochii1108@gmail.com>
2022-03-12 22:32:49 -0800

    Set input form

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './FormInput.css';
 
-export const FormInput = ({placeholder, setUserName}) => {
-  const inputHandler = (e) => {
-   setUserName(e.target.value)
-  }
+export const FormInput = ({label, onChange, id, ...others}) => {
+  // const inputHandler = (e) => {
+  //  setUserName(e.target.value)
+  // }
 
   return (
     <div className="formInput">
-      {/* <label>Username</label> */}
-      <input placeholder={placeholder} onChange={inputHandler} />
+      <label>{label}</label>
+      <input {...others} onChange={onChange}/>
     </div>
   )
 }
